c60a10642b64f7a5a8e111fda0310d3fe92b1da6
Mayanka Chattopadhyay <jodu892@gmail.com>
2021-08-23 16:28:11 +0530

    Added upload icon

--- src/pages/homepage/home.js
+++ src/pages/homepage/home.js
@@ -18,7 +18,25 @@ export default function Home(){
             <div className="container-fluid">
                 <div className="row">
                     <div className="col col-12 col-md-6">
-
+                        <div style={{marginBottom:'20px'}}>
+                        <button className="upload-btn">
+                            <i style={{color:'#0253B3'}} class="fas fa-file-upload fa-2x"></i>
+                        </button>
+                            
+                        </div>
+                        
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                            <label class="form-check-label" for="flexCheckDefault">
+                                Python
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked"/>
+                            <label class="form-check-label" for="flexCheckChecked">
+                                Java
+                            </label>
+                        </div>
                     </div>
                     <div className="col col-12 col-md-6 btn-div">
                         <button onClick={handleSubmit} className="submit-btn">Submit</button>
